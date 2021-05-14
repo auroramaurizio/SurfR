3b434f16408125ef3d4865bdfab8fbb21aa4209e
tascini.annasofia <volpesofi@gmail.com>
2021-05-14 13:15:00 +0200

    Added a test function and modified DESCRIPTION file

---
+++ R/Sum5.R
@@ -0,0 +1,11 @@
+#' Add 5 to any number
+#'
+#' @param input_num A number.
+#' @return The sum of \code{input_num} and 5.
+#' @examples
+#' Sum5(6)
+#'
+Sum5 <- function(input_num) {
+  a = input_num + 5
+  return(a)
+}
