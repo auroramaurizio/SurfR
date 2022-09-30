3421fc4345bc191efaf7e620ac75133f02561962
maurizio.aurora@hsr.it <maurizio.aurora@hsr.it>
2022-09-30 14:10:01 +0200

    nrep invnorm

--- R/metaRNAseq.R
+++ R/metaRNAseq.R
@@ -6,13 +6,14 @@
 #' @param test_statistic p-value combination technique (inverse normal or Fisher):
 #' \code{fishercomb}, \code{invnorm}.
 #' By default: \code{fishercomb}.
-#' @param BHth Benjamini Hochberg threshold. 
+#' @param BHth Benjamini Hochberg threshold.
+#' @param nrep Vector of numbers of replicates used in each study to calculate the previous one-sided p-values. 
 #' By default, the False Discovery Rate is controlled at 5%.
 #' @return A list with \code{DEindices} of DEG at the chosen Benjamini Hochberg threshold, and 
 #' \code{TestStatistic}, \code{rawpval}, \code{adjpval} vector for differential expression in the meta-analysis.
 #' @examples
 #' ind_deg = list(DEG1_df, DEG2_df, DEG3_df)
-#' comb_pval_df = metaRNAseq(ind_deg, test_statistic = "invnorm", BHth = 0.05)
+#' comb_pval_df = Meta(ind_deg, test_statistic = "invnorm", BHth = 0.05, nrep = c(2,2,2))
 #' @section Warning:
 #' Bla bla bla
 #' @family aggregate functions
@@ -20,7 +21,8 @@
 #' @export
 metaRNAseq <- function(ind_deg,
                  test_statistic = "fishercomb",
-                 BHth = 0.05) {
+                 BHth = 0.05,
+                 nrep = NULL) {
   
   import::here(metaRNASeq)
   
@@ -28,11 +30,9 @@ metaRNAseq <- function(ind_deg,
   
   histp  <- list()
   rawpval <- list()
-  nrep <-c()
   for (i in 1:length(ind_deg)){
     ind_deg[[i]] <- ind_deg [[i]][common_genes,]
     rawpval[[i]] <- ind_deg[[i]][["pvalue"]]
-    nrep[i] <- length(ind_deg[[i]])
     pdf(file = paste(names(ind_deg[i]),"_raw_pval_hist.pdf",sep ="", collapse = NULL))
     hist(rawpval[[i]], breaks=100, col="grey", main= names(ind_deg[i]), xlab="Raw p-values")
     dev.off()
