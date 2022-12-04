2d050ad918eaee78c46c71346a5638c7d7bf4309
maurizio.aurora@hsr.it <maurizio.aurora@hsr.it>
2022-12-04 19:13:38 +0100

    lib

--- R/Annotate_SPID.R
+++ R/Annotate_SPID.R
@@ -24,10 +24,10 @@ Annotate_SPID <- function(DGE, enrich.database  = "WikiPathway_2021_Human", outp
   import::here(MASS)
  
   annoname = enrich.database
-  annotation_table = enrichr_download(enrich.database)
+  annotation_table = hypeR::enrichr_download(enrich.database)
   annotation_table = as.data.frame(do.call(rbind, annotation_table)) #here gives a worning ignore it
   colNames = colnames(annotation_table) # could be any number of column names here
-  annotation_table['test']=col_concat(annotation_table, sep = " ")
+  annotation_table['test']=assertr::col_concat(annotation_table, sep = " ")
   annotation_table['GeneID'] <- trimws(annotation_table$test, which = c("both")) #remove whitespaces
   annotation_table$term = row.names(annotation_table)
   annotation_table_sub=annotation_table[,c("term","GeneID")]
