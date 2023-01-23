779eb462d75e7fefb29928c18dc4677ad613bbe3
tascini.annasofia <tascini.annasofia@unisr.it>
2023-01-23 17:05:36 +0100

    updated combine_fisher_invnorm

--- R/combine_fisher_invnorm.R
+++ R/combine_fisher_invnorm.R
@@ -6,7 +6,7 @@
 #' @param invnorm inverse normal p-value combination technique dataframe (output of metaRNAseq)
 #' @param fishercomb Fisher p-value combination technique dataframe (output of metaRNAseq)
 #' @param adjpval threshold to represent as binary the Meta-Analysis output adjpval.
-#' @return A dataframe with \code{DEindices} and \code{DEname} of DEG at the chosen Benjamini Hochberg threshold, and 
+#' @return A dataframe with \code{DEindices} and \code{DEname} of DEG at the chosen Benjamini Hochberg threshold, and
 #' \code{TestStatistic}, \code{rawpval}, \code{adjpval}, \code{binaryadjpval} vectors for differential expression in the meta-analysis.
 #' @examples
 #' ind_deg = list(DEG1_df, DEG2_df, DEG3_df)
@@ -24,39 +24,47 @@ combine_fisher_invnorm <- function(ind_deg,
                                    adjpval = 0.05,
                                    output_tsv = T,
                                    output_filename = "combine_fisher_invnorm.tsv") {
-  
-                          #import::here(metaRNASeq)
 
-                          DE = data.frame(genes=common_genes)
-                          FC = data.frame(genes=common_genes)
+  import::here(dplyr)
 
-                          for (i in 1:length(ind_deg)) {
-                              ind_deg[[i]] <- ind_deg [[i]][common_genes,]
-                              ind_deg[[i]][["binarypadj"]]=ifelse(ind_deg[[i]][["padj"]]<=adjpval,1,0)
-                              FC[[paste(names(ind_deg[i]), "log2FC", sep ="_")]] <- ind_deg[[i]][["log2FoldChange"]]
-                              DE[[paste(names(ind_deg[i]), "binarypadj", sep ="_")]] <- ind_deg[[i]][["binarypadj"]]
-                              }
+  common_genes = Reduce(intersect, lapply(ind_deg, rownames))
 
-                          FC <- FC[-c(1) ]
-                          DE <- DE[-c(1) ]
+  DE = data.frame(genes=common_genes)
+  FC = data.frame(genes=common_genes)
 
-                          signsFC <- mapply(FC, FUN=function(x) sign(x))
-                          sumsigns <- apply(signsFC,1,sum)
-                          commonsgnFC <- ifelse(abs(sumsigns)==dim(signsFC)[2], sign(sumsigns),0)
-                          FC$signFC <- commonsgnFC
+  for (i in 1:length(ind_deg)) {
+    ind_deg[[i]] <- ind_deg [[i]][common_genes,]
+    ind_deg[[i]][["binarypadj"]]=ifelse(ind_deg[[i]][["padj"]]<=adjpval,1,0)
+    FC[[paste(names(ind_deg[i]), "log2FC", sep ="_")]] <- ind_deg[[i]][["log2FoldChange"]]
+    DE[[paste(names(ind_deg[i]), "binarypadj", sep ="_")]] <- ind_deg[[i]][["binarypadj"]]
+  }
 
-                          comb <- cbind(FC,DE)
+  FC <- FC[-c(1) ]
+  DE <- DE[-c(1) ]
 
-                          comb$DE_fishercomb <- invnorm$binaryadjpval 
-                          comb$DE_invnorm <- fishercomb$binaryadjpval 
-                          comb$GeneID <- common_genes
-                          
-                            # -------- tsv --------
-                          if (output_tsv) {
-                          write.table(dgeResults, output_filename, quote = F, sep = "\t")
-                          }
- 
-                          return(comb)}
+  signsFC <- mapply(FC, FUN=function(x) sign(x))
+  sumsigns <- apply(signsFC,1,sum)
+  commonsgnFC <- ifelse(abs(sumsigns)==dim(signsFC)[2], sign(sumsigns),0)
+  FC$signFC <- commonsgnFC
+
+  comb <- cbind(FC,DE)
+
+  comb$DE_fishercomb <- invnorm$binaryadjpval
+  comb$DE_invnorm <- fishercomb$binaryadjpval
+  comb$GeneID <- common_genes
+
+  comb <- comb %>%  dplyr::relocate(GeneID)
+
+
+
+  # -------- tsv --------
+  if (output_tsv) {
+    write.table(comb, output_filename, quote = F, sep = "\t")
+  }
+
+  return(comb)
+
+}
 
 
 
