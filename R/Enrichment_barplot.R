9e91efee90709859f509f5a7f0c1db68e7777ba9
tascini.annasofia <tascini.annasofia@unisr.it>
2023-02-13 19:18:23 +0100

    updated R functions

--- R/Enrichment_barplot.R
+++ R/Enrichment_barplot.R
@@ -2,8 +2,9 @@
 #'
 #' Barplot representing the top up-regulated or down-regulated significant pathways
 #'
-#' @param dfList A list of enrichment tables for up and down-regulated genes in the different enrichrdatabases. Output of Enrichment.R function.
-#' @param enrich.databases Vector of EnrichR databases to consider 
+#' @param Enrich A list of enrichment tables for up and down-regulated genes in the different enrichR databases.
+#' Output of Enrichment.R function for one DGE experiment.
+#' @param enrich.databases Vector of EnrichR databases to consider. These databases must be present in the Enrich list.
 #' @param p_adj Double. Minimum Adjusted pvalue threshold for the enrichment
 #' @param num_term Double. Number of up-regulated and dw-regulated terms to represent
 #' @param cond String. Title of the plot.
@@ -14,30 +15,38 @@
 #' @family aggregate functions
 #' @seealso \code{\link{hello}} for counts data and metadata download, and \code{\link{hello}} for Gene2SProtein analysis
 #' @export
-#' 
+#'
 
 
-Enrichment_barplot <- function(dfList, enrich.databases  = c("GO_Biological_Process_2021",
+Enrichment_barplot <- function(Enrich, enrich.databases  = c("GO_Biological_Process_2021",
                                                      "GO_Cellular_Component_2021",
                                                      "GO_Molecular_Function_2021"), p_adj = 0.05, num_term = 10, cond = "UP") {
-     
-  
+
+
      import::here(stringr)
      import::here(openxlsx)
-     import::here(ggplot2)  
-  
+     import::here(ggplot2)
+
      enrichR.table = data.frame()
 
-     for (dat in enrich.databases) {
-     Table <- openxlsx::read.xlsx(xlsxFile=dfList, sheet=dat, startRow=1, colNames=T, rowNames=T,
-                               detectDates=F, skipEmptyRows=T, skipEmptyCols=T, na.strings="NA", fillMergedCells=F)
+     if (cond == "UP") {
+       enrich_list = Enrich[["fdr_up"]]
+     } else if (cond == "DOWN") {
+       enrich_list = Enrich[["fdr_down"]]
+     }
 
+     for (dat in enrich.databases) {
+     #Table <- openxlsx::read.xlsx(xlsxFile=dfList, sheet=dat, startRow=1, colNames=T, rowNames=T,
+     #                         detectDates=F, skipEmptyRows=T, skipEmptyCols=T, na.strings="NA", fillMergedCells=F)
+     Table <- enrich_list[[dat]]
      enrichR.table = rbind(enrichR.table, Table)
      }
-     
+
+     row.names(enrichR.table) <- enrichR.table$Term
+
      fx <- function(x) eval(parse(text=enrichR.table[x,]$Overlap))
 
-     p = row.names(enrichR.table[enrichR.table$Adjusted.P.value < p_adj,])
+     p = enrichR.table[enrichR.table$Adjusted.P.value < p_adj,"Term"]
 
      if(length(p)>0) { # Discarding the not-significant results (to avoid errors)
      pathways.dataframe = data.frame(Pathway=p, gene.ratio=sapply(p, fx), p.value=enrichR.table[p,]$P.value, p.value.adj=enrichR.table[p,]$Adjusted.P.value)
@@ -45,37 +54,39 @@ Enrichment_barplot <- function(dfList, enrich.databases  = c("GO_Biological_Proc
      pathways.dataframe = pathways.dataframe[order(pathways.dataframe$p.value.adj),]
      pathways.dataframe$Pathway.num = as.factor(dim(pathways.dataframe)[1]:1) # as.factor is somehow necessary to write the pathway names
      }
-     
+
      ###############################################
-     # N Top significant pathways 
+     # N Top significant pathways
      ###############################################
 
      top_sig = head(pathways.dataframe[1:num_term,], num_term)
-     
+
      ###############################################
      # Significant pathway barplot
      ###############################################
-     
+
      top_sig$Log10Adj.P.value = -log10(top_sig$p.value.adj)
-     
+
      #remove the description in brackets (GO:0071346)
-     top_sig$Pathway = str_replace(top_sig$Pathway, "\\s*\\([^\\)]+\\)", " ")
-     
-     top_sig <- top_sig[order(top_sig$gene.ratio),] 
-     
-     
-     p_top_sig <-ggplot(top_sig, aes(x=Pathway, y=gene.ratio, fill=Log10Adj.P.value)) +
-       geom_bar(stat="identity")+
-       scale_fill_gradient(low = "blue", high = "red",  name = "-Log10(p.adj.value)") + 
-       scale_x_discrete(limits = top_sig$Pathway)+
-       labs(title = cond) +
-       coord_flip()
-     
+     top_sig$Pathway = stringr::str_replace(top_sig$Pathway, "\\s*\\([^\\)]+\\)", " ")
+
+     top_sig <- top_sig[order(top_sig$gene.ratio),]
+
+
+     p_top_sig <- ggplot2::ggplot(top_sig, ggplot2::aes(x=Pathway, y=gene.ratio, fill=Log10Adj.P.value)) +
+       ggplot2::geom_bar(stat="identity")+
+       ggplot2::scale_fill_gradient(low = "blue", high = "red",  name = "-Log10(p.adj.value)") +
+       ggplot2::scale_x_discrete(limits = top_sig$Pathway)+
+       ggplot2::labs(title = cond) +
+       ggplot2::coord_flip()
+
      pdf("Top_sig_Pathways_barplot.pdf", 10, 5)
      p_top_sig
-     dev.off() 
-     
+     dev.off()
+
+     return(p_top_sig)
+
 }
-     
-    
+
+
 
--- R/Splot.R
+++ R/Splot.R
@@ -25,7 +25,7 @@ Splot <- function(SurfaceProteins_df,
 
 
   if (is.null(x = cols.use)) {
-    cols.use = hue_pal()(length(x = levels(as.factor(SurfaceProteins_df$v2plot))) )
+    cols.use = scales::hue_pal()(length(x = levels(as.factor(SurfaceProteins_df$v2plot))) )
     if (length(which (is.na(SurfaceProteins_df$v2plot)) > 0)) {
       warning("NA value in your dataframe")
       # Add grey for NA protein
@@ -36,17 +36,17 @@ Splot <- function(SurfaceProteins_df,
                "Be carefull to NA value"))
   }
 
-  plot <- ggplot(SurfaceProteins_df, aes(x=v2plot)) +
-    geom_bar(color = 'black', fill = cols.use )+
-    theme(plot.title = element_text(color="black", size=26, face="bold.italic"),
-          axis.text.x = element_text(angle = 45, face = "bold", color = "black", size=22, vjust = 1, hjust =1),
-          axis.title.x = element_text(face = "bold", color = "black", size = 24),
-          axis.text.y = element_text(angle = 0, face = "bold", color = "black", size=22),
-          axis.title.y = element_text(face = "bold", color = "black", size = 24),
-          legend.text = element_text(face = "bold", color = "black", size = 12),
-          legend.position="top",
-          panel.background = element_rect(fill = "white",colour = "black", size = 1, linetype = "solid")) +
-    labs(x = group.by, y = "N", title = main)
+  plot <- ggplot2::ggplot(SurfaceProteins_df, ggplot2::aes(x=v2plot)) +
+    ggplot2::geom_bar(color = 'black', fill = cols.use )+
+    ggplot2::theme(plot.title = ggplot2::element_text(color="black", size=26, face="bold.italic"),
+                   axis.text.x = ggplot2::element_text(angle = 45, face = "bold", color = "black", size=22, vjust = 1, hjust =1),
+                   axis.title.x = ggplot2::element_text(face = "bold", color = "black", size = 24),
+                   axis.text.y = ggplot2::element_text(angle = 0, face = "bold", color = "black", size=22),
+                   axis.title.y = ggplot2::element_text(face = "bold", color = "black", size = 24),
+                   legend.text = ggplot2::element_text(face = "bold", color = "black", size = 12),
+                   legend.position="top",
+                   panel.background = ggplot2::element_rect(fill = "white",colour = "black", size = 1, linetype = "solid")) +
+    ggplot2::labs(x = group.by, y = "N", title = main)
 
   ggplot2 <- ggrepel <- geom_label_repel <- hue_pal <- aes <- color <- shape <- sampleNames <- NULL
   return(plot)
